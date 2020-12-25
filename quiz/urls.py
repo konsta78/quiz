add50ae2b73ffad8b998a528f61295c2481d151d
konsta78 <panfilov2507@gmail.com>
2020-12-25 17:07:48 +0300

    реализована базовая страница; добавлена страница с 1 вопросом; ссылка с главной на первую; порезаны лишнии ссылки на доп. контент

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, QuizView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
+    path('quiz/', QuizView.as_view(), name='quiz'),
 
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 
+
 class IndexView(View):
     def get(self, request):
         return render(request, 'quiz/index.html')
+
+
+class QuizView(View):
+    def get(self, request):
+        return render(request, 'quiz/quiz.html')
