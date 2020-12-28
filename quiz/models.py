99d1ea2a8821ce686c39d930d1935f37442956f5
konsta78 <panfilov2507@gmail.com>
2020-12-28 20:08:38 +0300

    работа с БД через классы подключение admin панели создание модели Вопроса

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    ANSWERS = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+    )
+
+    number = models.IntegerField(verbose_name="Номер вопроса")
+    title = models.TextField(max_length=400, verbose_name="Текс вопроса")
+    ans1 = models.TextField(max_length=20, verbose_name="Вариант № 1")
+    ans2 = models.TextField(max_length=20, verbose_name="Вариант № 2")
+    ans3 = models.TextField(max_length=20, verbose_name="Вариант № 3")
+    ans4 = models.TextField(max_length=20, verbose_name="Вариант № 4")
+    r_ans = models.IntegerField(choices=ANSWERS, verbose_name="Парвильный ответ")
+
+    def __str__(self):
+        return "Вопрос № " + str(self.number)
+
+
