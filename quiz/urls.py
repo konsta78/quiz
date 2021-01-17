6958060698e737d4912a438cceac322d6ca85319
Konsta <panfilov2507@gmail.com>
2021-01-17 03:09:12 +0300

    очередной рефакторинг html... добавлена страница с результатами

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView, QuizView
+from .views import IndexView, QuizView, ResultsView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
     path('quiz/', QuizView.as_view(), name='quiz'),
+    path('results/', ResultsView.as_view(), name='results'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Question
 
@@ -10,6 +10,9 @@ class IndexView(View):
     """
     Отображение стартовой страницы
     """
+    user_r_a = 0
+    flag = 1
+
     def get(self, request):
         return render(request, 'quiz/index.html')
 
@@ -18,25 +21,42 @@ class QuizView(View):
     """
     Отображение страницы с вопросом и вариантами ответа
     """
+
     def check_num_question(self, request, template, **answer):
         num = request.GET.get("question")
+        img_style = 'height'
         if not num:
             num = 1
         else:
             num = int(num) + 1
             if num > num_of_questions["all_questions"]:
-                return render(request, 'quiz/index.html')
+                return redirect('results')
+            elif num in [5, 6]:
+                img_style = 'width'
 
-        context = {'question': Question.objects.get(pk=num), 'answer': answer}
+        context = {'question': Question.objects.get(pk=num), 'answer': answer, 'img_style': img_style}
         return render(request, template, context)
 
     def get(self, request):
+        IndexView.flag = 1
         num = self.check_num_question(request, 'quiz/quiz.html')
         return num
 
     def post(self, request):
-        answer = request.POST.get('ans_but')
+        if request.method == 'POST':
+            answer = request.POST.get('ans_but')
+            r = request.POST.get('right_answer')
+
+            if answer == r and IndexView.flag == 1:
+                IndexView.user_r_a += 1
+                IndexView.flag = 0
+
         num = self.check_num_question(request, 'quiz/answer.html', answer=answer)
         return num
 
 
+class ResultsView(View):
+    def get(self, request):
+        context = {'user_results': IndexView.user_r_a}
+        IndexView.user_r_a = 0
+        return render(request, 'quiz/results.html', context)
