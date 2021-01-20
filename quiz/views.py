19175a2f36837e4362bea03ad43aee7dc3ebe721
Konsta <panfilov2507@gmail.com>
2021-01-21 00:57:46 +0300

    Рабочая версия 1.0

--- quiz/views.py
+++ quiz/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .models import Question
 
+# словарь с правильными ответами
 RIGHT_ANSWERS = {
     1: '1', 2: '2', 3: '1', 4: '3', 5: '2',
     6: '3', 7: '4', 8: '2', 9: '4', 10: '2',
@@ -14,7 +15,7 @@ class IndexView(View):
     """
     Отображение стартовой страницы
     """
-    users_answers = {}
+    users_answers = {} # ответы пользователя
 
     def get(self, request):
         return render(request, 'quiz/index.html')
@@ -26,24 +27,28 @@ class QuizView(View):
     """
 
     def get(self, request):
-        token = request.headers['Cookie']
-        IndexView.users_answers[token] = {}
-        print("token -> ", token)
-
+        try:
+            # получаем токен пользователя и инициализируем его словпрь с ответами
+            token = request.headers['Cookie'].split('; ')[0].split('csrftoken=')[1]
+            IndexView.users_answers[token] = {}
+        except:
+            pass
         context = {'question': Question.objects.get(pk=1)}
         return render(request, 'quiz/quiz.html', context)
 
     def post(self, request):
-        num = int(request.POST.get("question"))
+        num = int(request.POST.get("question"))  # номер текущего вопроса
 
-        if request.POST.get('ans_but'):  # end of test
-            answer = request.POST.get('ans_but')
+        if request.POST.get('ans_but'):
+            answer = request.POST.get('ans_but')  # ответ пользователя (№ кнопки)
 
-            token = request.headers['Cookie']
-            print('user"s answers before -> ', IndexView.users_answers[token])
+            token = request.headers['Cookie'].split('; ')[0].split('csrftoken=')[1]
+
+            if token not in IndexView.users_answers:
+                IndexView.users_answers[token] = {}
             IndexView.users_answers[token][num] = answer
-            print('user"s answers after -> ', IndexView.users_answers[token])
 
+            # выбор шаблона для отображения ответа (по высоте или по ширине)
             if num in [5, 8, 9, 10, 12, 13, 14, 15, 16, 18, 19, 20]:
                 template = 'quiz/answer_w.html'
             else:
@@ -52,6 +57,7 @@ class QuizView(View):
             context = {'question': Question.objects.get(pk=num), 'answer': answer}
             return render(request, template, context)
 
+        # проверка окончания теста (всего 20 вопросов)
         num += 1
         if num > 20:
             return redirect('results')
@@ -61,10 +67,13 @@ class QuizView(View):
 
 
 class ResultsView(View):
+    """
+    Подсчет результатов и их отображение на странице
+    """
     def get(self, request):
 
-        user_right_answers = 0
-        token = request.headers['Cookie']
+        user_right_answers = 0  # счетчик правильных ответов
+        token = request.headers['Cookie'].split('; ')[0].split('csrftoken=')[1]
         user_answers = IndexView.users_answers[token]
 
         for i in range(1, 21):
@@ -72,4 +81,6 @@ class ResultsView(View):
                 user_right_answers += 1
 
         context = {'user_results': user_right_answers}
+        # удаление из общего словаря с ответами результаты текущего пользователя
+        del IndexView.users_answers[token]
         return render(request, 'quiz/results.html', context)
